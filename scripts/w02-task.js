67a3d99e9c9947dbff51a6c673e2634181296b19
Rene Marves <rfmarves@outlook.com>
2023-09-23 18:23:26 -0600

    completed w2 tasks

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,27 +3,35 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+let fullName = "René Marves";
+let currentYear = 2023;
+let profilePicture = "images/me.jpg"
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
-
-
-
+const favFoods = ["Kiupi", "Ceviche", "Shrimp", "Pizza"];
+foodElement.innerHTML += `<br>${favFoods}`;
+let foodItem = "Crêpes";
+favFoods.push(foodItem);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
+// for (let i = 0; i < favFoods.length; i++) {
+//     foodElement.innerHTML += `<br>${favFoods[i]}`;
+//   }
 
 
 
