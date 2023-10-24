b1aa5fea378ed788922524fdf6805cac92c77d36
Rene Marves <rfmarves@outlook.com>
2023-10-23 19:30:49 -0600

    test

--- scripts/project.js
+++ scripts/project.js
@@ -1,1 +1,23 @@
-Swatchy();
+import React, { Component } from 'react';
+import {jsx as _jsx} from 'react/jsx-runtime';
+
+import { useQuote } from '@marekdano/use-quote';
+
+const Example = () => {
+  const { quote, isLoading, isError } = useQuote()
+  
+  if (isLoading) return <p>Loading...</p>
+
+  if (quote) {
+    return (
+      <>
+        <p>{quote.text}</p>
+        <p><i>{quote.author}</i></p>
+      </>
+    )
+  }
+
+  if (isError) return <p>Difficulty displaying a quote at the moment.</p>
+
+  return null
+}
